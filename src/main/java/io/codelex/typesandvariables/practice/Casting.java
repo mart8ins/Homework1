50c6a58c6535752bfb986bf60813697db79bb8b2
mart8ins <mart8ins@gmail.com>
2023-05-06 09:03:29 +0300

    Finished Casting exrecise

--- src/main/java/io/codelex/typesandvariables/practice/Casting.java
+++ src/main/java/io/codelex/typesandvariables/practice/Casting.java
@@ -7,26 +7,26 @@ public class Casting {
     }
 
     private static void first() {
-        String a = "1";
-        Integer b = 2;
+        int a = 1;
+        int b = 2;
         int c = 3;
-        double d = 4;
-        float e = 5;
+        int d = 4;
+        int e = 5;
 
         //fixme - should be 15 :| 
-        //int sum = a + b + c + d + e;
-        //System.out.println(sum);
+        int sum = a + b + c + d + e;
+        System.out.println(sum);
     }
 
     private static void second() {
-        String a = "1";
-        Integer b = 2;
+        int a = 1;
+        int b = 2;
         int c = 3;
-        double d = 4.2;
+        float d = 4.2f;
         float e = 5.3f;
 
         //fixme - should be 15.5 :| 
-        //float sum = a + b + c + d + e;
-        //System.out.println(sum);
+        float sum = a + b + c + d + e;
+        System.out.println(sum);
     }
 }
