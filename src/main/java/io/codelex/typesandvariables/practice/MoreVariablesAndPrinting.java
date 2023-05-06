ca2fa040dbc0349f1193e58d851d066d34807aa7
mart8ins <mart8ins@gmail.com>
2023-05-06 08:59:06 +0300

    Finished MoreVariablesAndPrinting exrecise

--- src/main/java/io/codelex/typesandvariables/practice/MoreVariablesAndPrinting.java
+++ src/main/java/io/codelex/typesandvariables/practice/MoreVariablesAndPrinting.java
@@ -1,26 +1,39 @@
 package io.codelex.typesandvariables.practice;
 
+import java.util.Locale;
+
 public class MoreVariablesAndPrinting {
+    static double onePoundToKg = 0.453592d;
+    static double oneInchToCm = 2.54d;
+
+    public static double convertWeight(int w) {
+        return w * onePoundToKg;
+    }
+
+    public static double convertHeight(int h) {
+        return h * oneInchToCm;
+    }
+
     public static void main(String[] args) {
-        String myName, myEyes, myTeeth, myHair;
-        int myAge, myHeight, myWeight;
+        String name = "Zed A. Shaw";
+        String eyes = "Blue";
+        String teeth = "White";
+        String hair = "Brown";
+        int age = 35;
+        int height = 74; // inches
+        int weight = 180; // lbs
 
-        myName = "Zed A. Shaw";
-        myAge = 35;
-        myHeight = 74;  // inches
-        myWeight = 180; // lbs
-        myEyes = "Blue";
-        myTeeth = "White";
-        myHair = "Brown";
+        // Convert inches to centimeters and pounds to kilograms
+        double weightInKg = convertWeight(weight);
+        double heightInCm = convertHeight(height);
 
-        System.out.println("Let's talk about " + myName + ".");
-        System.out.println("He's " + myHeight + " inches tall.");
-        System.out.println("He's " + myWeight + " pounds heavy.");
+        System.out.println("Let's talk about " + name + ".");
+        System.out.printf("He's %.2f centimeters tall. \n", heightInCm);
+        System.out.printf("He's %.2f kilograms heavy. \n", weightInKg);
         System.out.println("Actually, that's not too heavy.");
-        System.out.println("He's got " + myEyes + " eyes and " + myHair + " hair.");
-        System.out.println("His teeth are usually " + myTeeth + " depending on the coffee.");
-
-        System.out.println("If I add " + myAge + ", " + myHeight + ", and " + myWeight
-                + " I get " + (myAge + myHeight + myWeight) + ".");
+        System.out.println("He's got " + eyes + " eyes and " + hair + " hair.");
+        System.out.println("His teeth are usually " + teeth + " depending on the coffee.");
+        
+        System.out.printf(Locale.US, "If I add %s, %.2f, and %.2f I get %.2f.", age, heightInCm, weightInKg, (age + heightInCm + weightInKg));
     }
 }
