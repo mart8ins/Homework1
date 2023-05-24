7b6e286e4da5771cf412ffb9fe367798fdf24272
mart8ins <mart8ins@gmail.com>
2023-05-24 19:54:26 +0300

    added input validation

--- src/main/java/io/codelex/enums/practice/exercise2/Exercise2.java
+++ src/main/java/io/codelex/enums/practice/exercise2/Exercise2.java
@@ -14,13 +14,18 @@ public class Exercise2 {
         while (true) {
             System.out.println("Your turn (Enter s for scissor, p for paper, t for stone, q to quit):");
             userInput = in.nextLine().toUpperCase();
-            if (userInput.equals("Q")) {
-                break;
+            if (userInput.matches("[S,P,T,Q]")) {
+                if (userInput.equals("Q")) {
+                    break;
+                }
+                String userWeapon = Game.getUserWeapon(userInput);
+                String computerWeapon = Game.getComputerWeapon();
+                String winner = Game.getWinner(userWeapon, computerWeapon);
+                System.out.println(winner);
+            } else {
+                System.out.println("Please enter correct values.");
             }
-            String userWeapon = Game.getUserWeapon(userInput);
-            String computerWeapon = Game.getComputerWeapon();
-            String winner = Game.getWinner(userWeapon, computerWeapon);
-            System.out.println(winner);
+
         }
         System.out.println(Game.getGameStats());
     }
