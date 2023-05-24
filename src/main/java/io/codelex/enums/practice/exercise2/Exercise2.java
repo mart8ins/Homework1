13b2547d899a4f0de21d84821175d216c57f03a7
mart8ins <mart8ins@gmail.com>
2023-05-24 10:47:06 +0300

    finished enums

---
+++ src/main/java/io/codelex/enums/practice/exercise2/Exercise2.java
@@ -0,0 +1,27 @@
+package io.codelex.enums.practice.exercise2;
+
+import java.util.Scanner;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String userInput;
+
+        System.out.println("Let us begin!");
+        System.out.println("Scissor-Paper-Stone");
+
+        while (true) {
+            System.out.println("Your turn (Enter s for scissor, p for paper, t for stone, q to quit):");
+            userInput = in.nextLine().toUpperCase();
+            if (userInput.equals("Q")) {
+                break;
+            }
+            String userWeapon = Game.getUserWeapon(userInput);
+            String computerWeapon = Game.getComputerWeapon();
+            String winner = Game.getWinner(userWeapon, computerWeapon);
+            System.out.println(winner);
+        }
+        System.out.println(Game.getGameStats());
+    }
+}
