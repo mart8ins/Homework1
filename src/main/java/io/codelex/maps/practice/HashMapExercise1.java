2a5e669c9ccef7d5ba52fe7186b8663ea662c411
mart8ins <mart8ins@gmail.com>
2023-05-30 10:59:03 +0300

    finished maps

--- src/main/java/io/codelex/maps/practice/HashMapExercise1.java
+++ src/main/java/io/codelex/maps/practice/HashMapExercise1.java
@@ -14,31 +14,34 @@ public class HashMapExercise1 {
         myMap.put(5, "Blue");
 
         //TODO: Print size of myMap
-        //System.out.println("Size of the hash map: " + ...);
+        System.out.println("Size of the hash map: " + myMap.size());
 
         //TODO: Check if myMap contains key 3
-        //if (?) {
-        //  System.out.println("Contains! :)");
-        //} else {
-        //  System.out.println("Does not contains. :(");
-        //}
+        if (myMap.containsKey(3)) {
+            System.out.println("Contains! :)");
+        } else {
+            System.out.println("Does not contains. :(");
+        }
 
         //TODO: Check if myMap contains value "White"
-        //if (?) {
-        //  System.out.println("Contains! :)");
-        //} else {
-        //  System.out.println("Does not contains. :(");
-        //}
+        if (myMap.containsValue("White")) {
+            System.out.println("Contains! :)");
+        } else {
+            System.out.println("Does not contains. :(");
+        }
 
         //TODO: Create new map and clone values from myMap into new map
-        //...
+        Map<Integer, String> newMap = new HashMap<>();
+        newMap.putAll(myMap);
+        System.out.println("Cloned map " + newMap);
 
         //TODO: Iterate over map entries and print keys and values of each entry
-        //for(Map.Entry entry: myMap.entrySet()){
-        //  System.out.println(?);
-        //}
+        for (Map.Entry entry : myMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", value: " + entry.getValue());
+        }
 
         //TODO: Clear myMap
+        myMap.clear();
         System.out.println("Hash map after clearing: " + myMap);
     }
 
